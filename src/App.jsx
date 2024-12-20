92ab710dcdcc97e9ee9708bffb1fc1c13c2fd5cc
Gautierpicon <gautierpicon@proton.me>
2024-12-20 22:46:20 +0100

    setup files and add react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import PropertyModal from "./components/PropertyModal.jsx";
+import FeaturedProperties from "./components/FeaturedProperties.jsx";
+import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./components/Home.jsx";
+
 function App() {
   return (
-    <div></div>
+    <div className="min-h-screen w-full bg-gray-50">
+      <Home />
+
+      <FeaturedProperties />
+
+      <Contact />
+
+      <Footer />
+    </div>
   );
 }
 
